6fb9a9317e981c03c1024d08b12dc36b92828d1d
JaceRogers12 <jacerogers12@gmail.com>
2024-03-13 16:02:42 -0600

    finished guided project

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,5 +1,6 @@
 import React from 'react'
 // We'll need quite a few imports from react-router-dom
+import {useParams, Link, Routes, Route} from "react-router-dom"
 
 import ItemDetails from './ItemDetails'
 
@@ -11,9 +12,9 @@ export default function Item(props) {
   // Beware! The ids are integers, whereas URL parameters are strings.
   // Beware! The JSX is expecting 'item' to exist instantly!
   // we need useParams to grab the dynamic parts of the path (:itemID).
-
-  const item = {}
-
+  const {id} = useParams()
+  let idNum = parseInt(id.slice(1,id.length))
+  const item = items.find(item => item.id === idNum)
   // This guards against a crash (the data is not available instantaneously)
   if (!items.length) return 'Getting your item...'
 
@@ -31,12 +32,15 @@ export default function Item(props) {
 
       <nav className='item-sub-nav'>
         {/* 👉 STEP 8 - Here go the NavLinks to `<current url>/shipping` and `<current url>/description` */}
-
+        <Link to="shipping">Shipping</Link>
+        <Link to="description">Description</Link>
       </nav>
-
       {/* 👉 STEP 9 - Here go the Routes for `<current path>/shipping` and `<current path>/description` */}
       {/* These Routes should render <ItemDetails />  */}
-
+        <Routes>
+          <Route path="shipping" element={<ItemDetails text={item.shipping}/>} />
+          <Route path="description" element={<ItemDetails text={item.description}/>} />
+        </Routes>
     </div>
   )
 }
